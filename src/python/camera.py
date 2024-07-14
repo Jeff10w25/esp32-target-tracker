44e190c15b347c6a1e11ce9dad3f053d1d6a2510
Thanathorn Suwanrasamee <thanatorns.2545@gmail.com>
2024-07-15 05:27:51 +0700

    added print concat frame and adjust saturation

--- src/python/camera.py
+++ src/python/camera.py
@@ -28,12 +28,26 @@ class Camera:
         except Exception as e:
             print(f"Error showing image. {e}")
             
+    def printTwoFrame(self, frame1: any, frame2: any) -> None:
+        result = cv2.hconcat([frame1, frame2])
+        try:            
+            cv2.imshow(self.windowName, result)
+        except Exception as e:
+            print(f"Error showing image. {e}")
+            
+    def changeSaturation(self, frame: any, value: int) -> any:
+        """ Increase saturation with positive value and decrease saturation with negative value """
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        hsv[:, 2, :] = cv2.add(hsv[:, 2, :], value)
+        frameAdjSat = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        return frameAdjSat
+            
     def changeBrightness(self, frame: any, value: int) -> any:
         """ Increase brightness with positive value and decrease brightness with negative value """
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         hsv[:, :, 2] = cv2.add(hsv[:, :, 2], value)
-        frameIncBright = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        return frameIncBright       
+        frameAdjBright = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+        return frameAdjBright       
 
     def destroy(self) -> None:
         self.cap.release()
